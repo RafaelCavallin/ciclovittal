2ed9d7ab675715533b4ff2b0eb7585b539d60e9d
Rafael Raszl <rafaelmsraszl@gmail.com>
2019-02-28 13:37:44 -0300

    ALTERADO BTN MODAL.

--- views-cache/index.e0d8db5e98e171957fed2b2fa14675d0.rtpl.php
+++ views-cache/index.e0d8db5e98e171957fed2b2fa14675d0.rtpl.php
@@ -309,7 +309,7 @@
 		        Obrigado!
 		      </div>
 		      <div class="modal-footer">
-		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+		        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
 		       <!--  <button type="button" class="btn btn-primary">Save changes</button> -->
 		      </div>
 		    </div>
